57d25bdf726a979e0a6a511f5b7a5fbb5b39dfaf
HederAlves <alvesheder92@gmail.com>
2022-07-08 00:21:34 -0300

    Update
    Get doctors

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
-        <h1>oi mundo</h1>
+        <h1>oi</h1>
+        <ul v-for="doctor in doctors" :key="doctor.id" >
+            <h1>{{ doctor.price }}</h1>
+        </ul>
     </div>
 </template>
 <script>
 export default {
-    name: "Schedule"
+    name: "Schedule",
+    
+     data() {
+        return {
+            doctors: [],
+        };
+    },
+    async mounted() {
+        const resp = await fetch('http://localhost:3000/doctors');
+        const response = await resp.json();
+        this.doctors = response.data;
+        console.log(response)
+    },
 }
 </script>
 <style>
-    
+
+ 
 </style>
