e28a30370420289ea688ae4bf358eede5f9101c1
HederAlves <alvesheder92@gmail.com>
2022-07-08 01:34:13 -0300

    Update
    Bug fix, in data consumption by API

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,15 +1,18 @@
 <template>
-    <div>
-        <h1>oi</h1>
-        <ul v-for="doctor in doctors" :key="doctor.id" >
-            <h1>{{ doctor.price }}</h1>
+    <div class="flex justify-around h-screen items-center">
+        <ul v-for="doctor of doctors" :key="doctor.id" >
+            <li>{{ doctor.name }}</li>
+            <li>{{ doctor.health_insurance }}</li>
+            <li>{{ doctor.data_query }}</li>
+            <li>{{ doctor.hours }}</li>
+            <li>{{ doctor.price }}</li>
+            <li>{{ doctor.users_query }}</li>
         </ul>
     </div>
 </template>
 <script>
 export default {
     name: "Schedule",
-    
      data() {
         return {
             doctors: [],
@@ -18,12 +21,7 @@ export default {
     async mounted() {
         const resp = await fetch('http://localhost:3000/doctors');
         const response = await resp.json();
-        this.doctors = response.data;
-        console.log(response)
+        this.doctors = response;
     },
 }
 </script>
-<style>
-
- 
-</style>
