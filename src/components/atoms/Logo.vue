07dfdde9b4dc5ef7ea887688cb7088cba14b7387
HederAlves <alvesheder92@gmail.com>
2022-07-12 17:19:17 -0300

    Create
    - Componets Atoms

---
+++ src/components/atoms/Logo.vue
@@ -0,0 +1,13 @@
+<template>
+  <img src="public/icon/logo-med.png" alt="Logo marca da empresa" />
+</template>
+<script>
+export default {
+  name: "Logo",
+};
+</script>
+<style>
+img {
+  @apply w-36 self-center;
+}
+</style>
