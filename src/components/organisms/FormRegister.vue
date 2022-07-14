8302a95f752762ee3766ae4fb9012aa7e1b2b012
HederAlves <alvesheder92@gmail.com>
2022-07-14 04:24:10 -0300

    Update
    Components of form:
    -FormDependents
    -FormRegister
    -FormSchedule

---
+++ src/components/organisms/FormRegister.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <form @submit.prevent="salvar">
+      <h1 class="text-center font-bold xl:mt-0">Faça seu cadastro</h1>
+      <label>Nome</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="Nome"
+        v-model="user.name"
+      />
+      <label>Data de Nascimento</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="29/05/1992"
+        v-model="user.data_birth"
+      />
+      <label>Sexo</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="Masculino"
+        v-model="user.sex"
+      />
+      <label>Telefone</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="(99)9.9999-9999"
+        v-model="user.phone"
+      />
+      <label>Nacionalidade</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="Brasileiro"
+        v-model="user.nationality"
+      />
+      <label>CPF</label>
+      <input
+        class="inputPrimary"
+        type="text"
+        placeholder="000.000.000-00"
+        v-model="user.CPF"
+      />
+      <button class="button">salvar</button>
+    </form>
+    <Modal v-if="visible"
+      ><h1>Cadastrado com sucesso!</h1>
+      <p>Deseja cadastrar dependentes?</p></Modal
+    >
+  </div>
+</template>
+<script>
+import Users from "../../services/users";
+import Modal from "../atoms/Modal.vue";
+
+export default {
+  name: "FormRegister",
+  data() {
+    return {
+      visible: false,
+      user: {
+        id: "",
+        name: null,
+        data_birth: null,
+        sex: null,
+        phone: null,
+        nationality: null,
+        CPF: null,
+      },
+    };
+  },
+  methods: {
+    salvar() {
+      if (!this.user.id) {
+        Users.salvar(this.user)
+          .then((_resposta) => {
+            this.user = {};
+            this.visible = true;
+          })
+          .catch((e) => {
+            this.errors = e.response.data.errors;
+          });
+      }
+    },
+  },
+  components: { Modal },
+};
+</script>
+<style>
+form {
+  @apply flex flex-col gap-2;
+}
+
+.button {
+  @apply w-[50vh] h-[6vh] font-bold rounded-md  pl-2 bg-emerald-400 my-5;
+}
+</style>
---
+++ src/components/organisms/FormSchedule.vue
@@ -0,0 +1,147 @@
+<template>
+  <div>
+    <form @submit.prevent="salvar">
+      <ul>
+        <li>
+          <label>Selecione o Medico</label>
+          <select name="name" id="name" v-model="schedule.name">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.name"
+            >
+              {{ doctor.name }}
+            </option>
+          </select>
+        </li>
+        <li>
+          <label>Selecione o Convênio</label>
+          <select name="name" id="name" v-model="schedule.health_insurance">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.health_insurance"
+            >
+              {{ doctor.health_insurance }}
+            </option>
+          </select>
+        </li>
+        <li>
+          <label>Selecione a Data</label>
+          <select name="name" id="name" v-model="schedule.data_query">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.data_query"
+            >
+              {{ doctor.data_query }}
+            </option>
+          </select>
+        </li>
+        <li>
+          <label>Selecione o Horário de Atendimento</label>
+          <select name="name" id="name" v-model="schedule.hours">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.hours"
+            >
+              {{ doctor.hours }}
+            </option>
+          </select>
+        </li>
+        <li>
+          <label>Selecione o Medico</label>
+          <select name="name" id="name" v-model="schedule.price">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.price"
+            >
+              {{ doctor.price }}
+            </option>
+          </select>
+        </li>
+        <li>
+          <label>Selecione para quem é a consulta</label>
+          <select name="name" id="name" v-model="schedule.users_query">
+            <option
+              v-for="doctor of doctors"
+              :key="doctor.id"
+              :value="doctor.users_query.name_dependente"
+            >
+              {{ doctor.users_query.name_dependente }}
+            </option>
+          </select>
+        </li>
+      </ul>
+      <button class="button">Agendar</button>
+    </form>
+  </div>
+</template>
+<script>
+import Doctor from "../../services/doctors";
+
+export default {
+  name: "FormSchedule",
+  data() {
+    return {
+      doctors: [],
+      schedule: {
+        id: null,
+        name: null,
+        health_insurance: null,
+        data_query: null,
+        hours: null,
+        price: null,
+      },
+    };
+  },
+  mounted() {
+    this.listar();
+  },
+  methods: {
+    listar() {
+      Doctor.listar()
+        .then((response) => {
+          this.doctors = response.data;
+          console.log(response.data);
+          this.names = response;
+          this.health_insurance = response;
+          this.data_query = response;
+          this.hours = response;
+          this.price = response;
+          this.users_query = response;
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    salvar() {
+      if (!this.schedule.id) {
+        Schedule.salvar(this.schedule)
+          .then((_resposta) => {
+            this.schedule = {};
+            alert("Agendado com sucesso!");
+          })
+          .catch((e) => {
+            this.errors = e.response.data.errors;
+          });
+      }
+    },
+  },
+};
+</script>
+<style>
+li {
+  @apply flex flex-col gap-2;
+}
+
+select {
+  @apply w-[50vh] border-4 rounded-md pl-2;
+}
+
+.button {
+  @apply w-[50vh] h-[6vh] font-bold rounded-md  pl-2 bg-emerald-400 my-5;
+}
+</style>
