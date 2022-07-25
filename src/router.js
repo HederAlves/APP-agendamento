ee09e020babf4c1361174c192e01f871d7f493a6
HederAlves <alvesheder92@gmail.com>
2022-07-25 00:59:00 -0300

    Create
    New route

--- src/router.js
+++ src/router.js
@@ -1,23 +1,32 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Schedule from './views/Schedule.vue'
-import Register from './views/Register.vue'
-import Dependents from './views/Dependents.vue'
+import Dependents from "./views/Dependents.vue";
+import Schedule from "./views/Schedule.vue";
+import Register from "./views/Register.vue";
+import Login from "./views/Login.vue";
 
- const routes = [{
-    path: '/agendamento',
-    name: 'schedule',
-    component: Schedule
-},
-{
-    path: '/cadastro',
-    name: 'register',
-    component: Register
-},
-{
-    path: '/dependentes',
-    name: 'dependents',
-    component: Dependents
-}]
+const routes = [
+  {
+    path: "/",
+    name: "login",
+    component: Login,
+  },
+
+  {
+    path: "/agendamento",
+    name: "schedule",
+    component: Schedule,
+  },
+  {
+    path: "/cadastro",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/dependentes",
+    name: "dependents",
+    component: Dependents,
+  },
+];
 
 const router = createRouter({ history: createWebHistory(), routes });
 
