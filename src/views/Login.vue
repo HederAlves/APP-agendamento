f001d99a5a220e40ce404b548b987d89e8864ca1
HederAlves <alvesheder92@gmail.com>
2022-07-25 00:59:13 -0300

    Create
    New component

---
+++ src/views/Login.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="flex flex-col mt-5 justify-center ml-1 gap-5 h-screen items-center sm:ml-0 sm:mt-0 sm:flex-row sm:justify-start sm:gap-20 md:gap-28 xl:gap-64"
+  >
+    <ImageLoginRegister />
+    <IconLogo />
+    <FormLogin />
+  </div>
+</template>
+<script>
+import IconLogo from "../components/atoms/IconLogo.vue";
+import FormLogin from "../components/organisms/FormLogin.vue";
+import ImageLoginRegister from "../components/atoms/ImageLoginRegister.vue";
+
+export default {
+  name: "Login",
+  components: { IconLogo, ImageLoginRegister, FormLogin }
+};
+</script>
