7e648b323348483c8c136c3c40d796f4c53dc57a
Heder Alves <alvesheder92@gmail.com>
2022-10-18 23:00:32 -0300

    Update

--- src/views/Schedule.vue
+++ src/views/Schedule.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col mt-5 justify-center ml-1 gap-5 h-screen items-center sm:ml-0 sm:mt-0 sm:flex-row sm:justify-start sm:gap-20 md:gap-28 xl:gap-64">
+  <div class="flex flex-col mt-24 justify-center ml-1 gap-5 h-screen items-center sm:ml-0 sm:mt-0 sm:flex-row sm:justify-start sm:gap-20 md:gap-28 xl:gap-64">
     <ImageSchedule />
     <IconLogo />
     <FormSchedule />
