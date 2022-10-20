557cdd7775e6c47b4a65177f04e211398c89c041
Heder Alves <alvesheder92@gmail.com>
2022-10-19 22:53:30 -0300

    Update

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex flex-col mt-5 justify-center ml-1 gap-5 h-screen items-center sm:ml-0 sm:mt-0 sm:flex-row sm:justify-start sm:gap-20 md:gap-28 xl:gap-64"
+    class="flex flex-col mt-32 justify-center ml-1 gap-10 h-screen items-center sm:ml-0 sm:mt-0 sm:flex-row sm:justify-start sm:gap-20 md:gap-28 xl:gap-64"
   >
     <ImageLoginRegister />
     <IconLogo />
